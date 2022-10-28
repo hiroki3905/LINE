0ab7824f7062661474f4eafe20bac91fc5c50210
hiroki3905 <96215671+hiroki3905@users.noreply.github.com>
2022-10-28 22:33:44 +0900

    メソッド引数にタグ追加

--- src/main/java/packag/Fujii.java
+++ src/main/java/packag/Fujii.java
@@ -18,7 +18,8 @@ public class Fujii {
 	public static void main(String args[]) throws Exception{
 		List<String> sample_list = new ArrayList<String>();
 		String url = "https://iss.ndl.go.jp/api/sru?operation=searchRetrieve&query=title%3d%22%E3%83%97%E3%83%AD%E3%82%B0%E3%83%A9%E3%83%9F%E3%83%B3%E3%82%B0%22&recordPacking=xml&recordSchema=dcndl";
-		System.out.println(getXMLContents(10,sample_list,url));
+		String tag = "dcterms:title";
+		System.out.println(getXMLContents(10,sample_list,tag,url));
 	}
     
  
@@ -27,12 +28,12 @@ public class Fujii {
      *XMLからコンテンツを取得するメソッド
      *(取り出す要素数,データを入れるリスト,url)
     */
-    public static List<String> getXMLContents(int i,List<String> p,String url) throws SAXException, IOException, ParserConfigurationException{
+    public static List<String> getXMLContents(int i,List<String> p,String tag,String url) throws SAXException, IOException, ParserConfigurationException{
 
     		
             Document document= DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(url);
             Element rootElement=document.getDocumentElement();
-            NodeList nodeList = rootElement.getElementsByTagName("dcterms:title");
+            NodeList nodeList = rootElement.getElementsByTagName(tag);
 
             for(int x = 0;x <i;x++) {
             Element elem = (Element)nodeList.item(x);
