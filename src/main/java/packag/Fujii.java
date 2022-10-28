ddd1dd39f14ef51392d4f2975a8cc93aebccb0bf
hiroki3905 <96215671+hiroki3905@users.noreply.github.com>
2022-10-28 22:31:10 +0900

    メソッド引数にurlを追加

--- src/main/java/packag/Fujii.java
+++ src/main/java/packag/Fujii.java
@@ -17,19 +17,20 @@ public class Fujii {
 	
 	public static void main(String args[]) throws Exception{
 		List<String> sample_list = new ArrayList<String>();
-		System.out.println(getXMLContents(10,sample_list));
+		String url = "https://iss.ndl.go.jp/api/sru?operation=searchRetrieve&query=title%3d%22%E3%83%97%E3%83%AD%E3%82%B0%E3%83%A9%E3%83%9F%E3%83%B3%E3%82%B0%22&recordPacking=xml&recordSchema=dcndl";
+		System.out.println(getXMLContents(10,sample_list,url));
 	}
     
  
 
     /*
      *XMLからコンテンツを取得するメソッド
-     *(取り出す要素数,データを入れるリスト)
+     *(取り出す要素数,データを入れるリスト,url)
     */
-    public static List<String> getXMLContents(int i,List<String> p) throws SAXException, IOException, ParserConfigurationException{
+    public static List<String> getXMLContents(int i,List<String> p,String url) throws SAXException, IOException, ParserConfigurationException{
 
     		
-            Document document= DocumentBuilderFactory.newInstance().newDocumentBuilder().parse("https://iss.ndl.go.jp/api/sru?operation=searchRetrieve&query=title%3d%22%E3%83%97%E3%83%AD%E3%82%B0%E3%83%A9%E3%83%9F%E3%83%B3%E3%82%B0%22&recordPacking=xml&recordSchema=dcndl");
+            Document document= DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(url);
             Element rootElement=document.getDocumentElement();
             NodeList nodeList = rootElement.getElementsByTagName("dcterms:title");
 
